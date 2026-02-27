74829a90320a668d88b1d3d3d34df5c36e231797
satan <leejs8266@gmail.com>
2026-02-27 23:28:45 +0900

    feat: add class 4 problems

--- class_4/problem_15686.py
+++ class_4/problem_15686.py
@@ -1,21 +1,37 @@
+from itertools import combinations
+
 n, m = map(int, input().split())
 
 homes = []
 chickens = []
 for i in range(n):
-    row = list(map(int, input().split()))
+    line = list(map(int, input().split()))
     for j in range(n):
-        if row[j] == 1:
+        if line[j] == 1:
             homes.append((i, j))
-        elif row[j] == 2:
+        elif line[j] == 2:
             chickens.append((i, j))
 
-stack = []
-for i in range(len(chickens)):
+
+def cal_distance(home, chicken):
+    return abs(home[0] - chicken[0]) + abs(home[1] - chicken[1])
 
 
-while stack:
-    v = stack.pop()
+distance = []
+for h in homes:
+    row = []
+    for c in chickens:
+        row.append(cal_distance(h, c))
+    distance.append(row)
 
-    for i in range(len(chickens)):
-        continue
+result = float("inf")
+for comb in combinations(range(len(chickens)), m):
+    current = 0
+    for h in distance:
+        chicken_distance = int(1e9)
+        for c in comb:
+            chicken_distance = min(chicken_distance, h[c])
+        current += chicken_distance
+    result = min(result, current)
+
+print(result)
